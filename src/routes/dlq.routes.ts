597b104880ef95484c873a658a8772181e6f004c
Atul Kumar Jha <atuljha21542@gmail.com>
2026-04-01 16:30:04 +0530

    Removed auth middleware from merchant and DLq

--- src/routes/dlq.routes.ts
+++ src/routes/dlq.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { dlqController } from '../controllers/dlq.controller.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 router.get('/', dlqController.getAllDeadEvents);
 router.get('/:eventId', dlqController.getDeadEventById);
-router.post('/:eventId/requeue', authMiddleware, dlqController.requeueEvent);
+router.post('/:eventId/requeue', dlqController.requeueEvent);
 
 export default router;
--- src/routes/merchant.routes.ts
+++ src/routes/merchant.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 import { merchantController } from '../controllers/merchant.controller.js';
-import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 router.post('/register', merchantController.register);
 router.get('/:id', merchantController.getById);
-router.patch('/:id/toggle', authMiddleware, merchantController.toggle);
+router.patch('/:id/toggle', merchantController.toggle);
 
 export default router;
