306804d0e30f358519f1fc08492a4744c8b8aaf1
Atul Kumar Jha <atuljha21542@gmail.com>
2026-04-02 13:54:14 +0530

    Added a simple landing page

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-// GET - for browser visits
+// for browser visits
 app.get('/test-receiver', (_req: Request, res: Response) => {
     return res.status(200).json({
         message: 'Hermes test receiver is live',
@@ -30,7 +30,7 @@ app.get('/test-receiver', (_req: Request, res: Response) => {
     });
 });
 
-// POST - for actual webhook delivery
+// for actual webhook delivery
 app.post('/test-receiver', (req: Request, res: Response) => {
     console.log('Test receiver hit:', {
         event: req.headers['x-webhook-event'],
@@ -55,6 +55,9 @@ app.get('/health', (_req: Request, res: Response) => {
     });
 });
 app.use('/dashboard', express.static(process.cwd() + '/src/public'));
+app.get('/', (_req: Request, res: Response) => {
+  res.sendFile(process.cwd() + '/src/public/landing.html');
+});
 
 //API Routes
 app.use('/api/merchants', merchantRoutes);
